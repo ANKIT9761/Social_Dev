4011c04908acb24d3605faf0b5d193ab50bfb615
Ankit Kanyal <ankitsinghkanyal43@gmail.com>
2022-11-24 16:49:13 +0530

    Done with left section ProfileSide of the app

---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProfileSide from '../../profileSide/ProfileSide'
+import './Home.css'
+const Home = () => {
+  return (
+    <div className='Home'>
+        <ProfileSide/>
+        <div className='postSide'>Posts</div>
+        <div className='RightSide'>Rightside</div>
+    </div>
+  )
+}
+
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./App";
 
-ReactDOM.render(
+
+const container = document.getElementById('root');
+const root=createRoot(container);
+
+root.render(
   <App/>,
-  document.getElementById("root")
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
